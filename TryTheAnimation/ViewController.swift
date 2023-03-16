38d867781c2ec6a0a18f6c1b4fd298eca45aed08
Kirill Kniga <kiruha_95@icloud.com>
2023-03-16 17:04:41 +0300

    Initial Commit

---
+++ TryTheAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryTheAnimation
+//
+//  Created by Kirill Kniga on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
